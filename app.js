c5d0b91914b6b6023766a41390b7d75381e56e13
amrrdev <amrrdev@gmail.com>
2024-08-07 20:42:10 +0300

    final

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-import path from "node:path";
-
 import gloablErrorHandlerMiddleware from "./middlewares/gloablErrorHandler.js";
 import productRouter from "./routes/productRoutes.js";
 import AppError from "./utils/appError.js";
 
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
 import express from "express";
+import morgan from "morgan";
 import fileUpload from "express-fileupload";
+import { v2 as cloudinary } from "cloudinary";
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.static("./public"));
-// parse json
 app.use(express.json());
 
 // parse multipart/form data
-app.use(fileUpload({ createParentPath: true }));
+app.use(fileUpload({ useTempFiles: true }));
+
+console.log(process.env.CLOUDINARY_NAME);
+console.log(process.env.CLOUDINARY_API_KEY);
+console.log(process.env.CLOUDINARY_API_SECRET);
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.get("/", (req, res, next) => {
     res.send("<h1>Hello motherfucker</h1>");

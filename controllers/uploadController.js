d27211c6713a110165426ca42dd201096e8a6a97
amrrdev <amrrdev@gmail.com>
2024-08-07 21:29:00 +0300

    BOY

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -8,28 +8,6 @@ import AppError from "../utils/appError.js";
 
 import { v2 as cloudinary } from "cloudinary";
 
-// upload images and store it locally
-export const uploadProductImageLocal = catchAsync(async (req, res, next) => {
-    if (!req.files) return next(new AppError("No Image Uploaded!", StatusCodes.BAD_REQUEST));
-
-    const productImage = req.files.image;
-
-    if (!productImage.mimetype.startsWith("image"))
-        return next(new AppError("Please Upload Image!", StatusCodes.BAD_REQUEST));
-
-    const MAX_SIZE = 1024 * 1024;
-
-    if (productImage.size > MAX_SIZE)
-        return next(new AppError("Please upload image smaller 1MB", StatusCodes.BAD_REQUEST));
-
-    const imagePath = path.join(
-        import.meta.dirname,
-        "../public/uploads/" + `${productImage.names}`
-    );
-    await productImage.mv(imagePath);
-    res.status(StatusCodes.OK).json({ image: { src: `/uploads/${productImage.name}` } });
-});
-
 export const uploadProductImage = catchAsync(async (req, res, next) => {
     // Check if any files were uploaded
     if (!req.files || !req.files.image) {
@@ -66,4 +44,26 @@ export const uploadProductImage = catchAsync(async (req, res, next) => {
     });
 
     return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
+});
+
+// Use this function if you want to store the images locally on the server
+export const uploadProductImageLocal = catchAsync(async (req, res, next) => {
+    if (!req.files) return next(new AppError("No Image Uploaded!", StatusCodes.BAD_REQUEST));
+
+    const productImage = req.files.image;
+
+    if (!productImage.mimetype.startsWith("image"))
+        return next(new AppError("Please Upload Image!", StatusCodes.BAD_REQUEST));
+
+    const MAX_SIZE = 1024 * 1024;
+
+    if (productImage.size > MAX_SIZE)
+        return next(new AppError("Please upload image smaller 1MB", StatusCodes.BAD_REQUEST));
+
+    const imagePath = path.join(
+        import.meta.dirname,
+        "../public/uploads/" + `${productImage.names}`
+    );
+    await productImage.mv(imagePath);
+    res.status(StatusCodes.OK).json({ image: { src: `/uploads/${productImage.name}` } });
 });
